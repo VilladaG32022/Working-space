fe11677df59bd522031f43e22539739dcdc52229
VilladaG32022 <proyectosolidariovillada@gmail.com>
2022-06-23 11:54:44 -0300

    updates 23/06/22 11:55
    Nav Animation and Change name of main.js

---
+++ Codes/JS/main.js
@@ -0,0 +1,69 @@
+/*=============== SWIPER ===============*/
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
+
+
+/*=============== NAV ANIMATION ===============*/
+
+
+const navIcons = document.querySelectorAll('.nav__icon');
+const navLinks = document.querySelectorAll('.nav__link');
+const navItems = document.querySelectorAll('.nav__item');
+
+const addListeners = (item, icon, link) => {
+  const navIcon = document.getElementById(icon);
+  const navLink = document.getElementById(link);
+
+  
+  navIcon.addEventListener('mouseover', () =>{
+    navLink.classList.remove('nav__link-hidden')
+    navLink.classList.add('animate__animated','animate__bounceIn')
+  })
+  navIcon.addEventListener('mouseout', () =>{
+    navLink.classList.add('nav__link-hidden')
+    navLink.classList.remove('animate__animated','animate__bounceIn')
+  })
+  
+  navLink.addEventListener('mouseover', () =>{
+    navLink.classList.remove('nav__link-hidden')
+  })
+  navLink.addEventListener('mouseout', () =>{
+    navLink.classList.add('nav__link-hidden')
+  })
+}
+
+navIcons.forEach((ni) =>{
+  switch (ni.id) {
+    case 'icon-home':
+      addListeners('item-home', 'icon-home', 'link-home')
+      break;
+    case 'icon-about':
+      addListeners('item-about', 'icon-about', 'link-about')
+      break;
+    case 'icon-volunteering':
+      addListeners('item-volunteering', 'icon-volunteering', 'link-volunteering')
+      break;
+    case 'icon-update':
+      addListeners('item-update', 'icon-update', 'link-update')
+      break;
+    }
+  }
+)
